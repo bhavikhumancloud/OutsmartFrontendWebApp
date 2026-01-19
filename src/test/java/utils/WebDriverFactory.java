61ed20ffba745b9b7d3da833685f5ccc05b92651
Bhavikmohod <bhavik.mohod@humancloud.co.in>
2026-01-19 12:15:11 +0530

    updated the framework

--- src/test/java/utils/WebDriverFactory.java
+++ src/test/java/utils/WebDriverFactory.java
@@ -3,6 +3,10 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class WebDriverFactory {
 
@@ -11,7 +15,14 @@ public class WebDriverFactory {
     // Create and register a ChromeDriver for the current thread
     public static WebDriver createDriver() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        Map<String, Object> prefs = new HashMap<>();
+        // 0 = default, 1 = allow, 2 = block
+        prefs.put("profile.default_content_setting_values.geolocation", 1);
+        options.setExperimentalOption("prefs", prefs);
+
+        WebDriver driver = new ChromeDriver(options);
         DRIVER.set(driver);
         return driver;
     }
